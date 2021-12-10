dded5df32d3fd8fdf4540877495e764d6cd218ba
Sergio Pachon Dotor <sap9827@gmail.com>
2021-12-10 00:18:41 -0500

    Another Update

--- app/src/main/java/co/edu/unab/icareapp20/MainActivity.java
+++ app/src/main/java/co/edu/unab/icareapp20/MainActivity.java
@@ -1,22 +1,40 @@
 package co.edu.unab.icareapp20;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+        Button signUpBtn = findViewById(R.id.signUpButton);
+        Button logInBtn = findViewById(R.id.loginButton);
 
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this,"Holy",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this,"Holy",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, LoggedInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
 }
--- app/src/main/java/co/edu/unab/icareapp20/SignUpActivity.java
+++ app/src/main/java/co/edu/unab/icareapp20/SignUpActivity.java
@@ -2,7 +2,10 @@ package co.edu.unab.icareapp20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        Button signUpBtn = findViewById(R.id.submitButton);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this,"Holy",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
