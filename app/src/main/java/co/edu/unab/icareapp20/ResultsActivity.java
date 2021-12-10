d55b0dc369a315f59d68fc2d9bc3bc0099d943cf
Sergio Pachon Dotor <sap9827@gmail.com>
2021-12-10 00:23:47 -0500

    Started on GitHub!

---
+++ app/src/main/java/co/edu/unab/icareapp20/ResultsActivity.java
@@ -0,0 +1,14 @@
+package co.edu.unab.icareapp20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
+}
